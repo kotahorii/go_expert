aa0c77733a2532648725fdb58f84fd146d196b0a
kotahorii <khorii.micron@gmail.com>
2022-03-15 14:05:16 +0900

    [update] main.bo

--- main.go
+++ main.go
@@ -4,21 +4,41 @@ import (
 	"log"
 	"sync"
 	"time"
+
+	"golang.org/x/sync/singleflight"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(n int) {
-			defer wg.Done()
-			do(n)
-		}(i)
-	}
-	wg.Wait()
+	var g group
+
+	g.do("foo")
+	g.do("bar")
+	g.Wait()
+
+	g.Forget()
+	g.do("hoge")
+
+	g.do("fuga")
+	g.Wait()
 }
 
-func do(n int) {
-	time.Sleep(1 * time.Second)
-	log.Printf("%d called", n)
+type group struct {
+	singleflight.Group
+	sync.WaitGroup
 }
+
+func (g *group) do(s string) {
+	g.Add(1)
+	go func() {
+		defer g.Done()
+
+		v, err, shared := g.Do("key", func() (interface{}, error) {
+			time.Sleep(1 * time.Second)
+			log.Printf("cached %s\n", s)
+			return s, nil
+		})
+		log.Println(v, err, shared)
+	}()
+}
+
+func (g *group) Forget() { g.Group.Forget("key") }
